fc5adaeff669afc530bbf795deaa94c4acf96fa9
Moses Kivua <kivuamoses9@gmail.com>
2022-08-22 23:29:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//arrow functions
+ function divide(number1 = 2000, number2 = 100) {
+    let result = number1 / number2;
+    return result;
+}
+console.log(divide());
+const square = number => number {
+    return number*number;
+}
+const add = (number1, number2) => number1+ number2;
